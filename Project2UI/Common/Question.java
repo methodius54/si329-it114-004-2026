fe50cb587739f00cd9323970f73a25ef70361010
stefan <stefanusa20@gmail.com>
2026-05-08 00:32:17 -0400

    minor clarification

--- Project2UI/Common/Question.java
+++ Project2UI/Common/Question.java
@@ -3,6 +3,10 @@ package Project2UI.Common;
 import java.util.List;
 
 public class Question {
+    // This is a bit strange, but each different class, Question, QuestionPayload, QAPayload each serve a respective purpose
+    // QAPayload does the job of handling server-toclient
+    // QuestionPayload does the job of handling client-toserver
+    // Question does the job of handling server-side
     private String question;
     private String category;
     private List<String> options;
